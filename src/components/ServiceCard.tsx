e112c0df25f6df7cc66f74a9a0efdee77bc393e5
analistaRoberto <analistaroberto.pena@gmail.com>
2025-07-01 23:02:06 -0400

    solucionesrp_qa_v0.2

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -7,7 +7,7 @@ interface ServiceCardProps {
   color: string;
 }
 
-const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, icon, color }) => {
+const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, icon }) => {
   return (
     <div className={`bg-[#f4a124]/80 rounded-xl p-3 hover:bg-[#f4a124]/90 transition-all duration-300 border border-[#f4a124]/50 hover:border-[#f4a124] flex flex-col justify-center h-[200px] max-w-[200px] shadow-lg hover:shadow-xl`}>
       <div className="text-xl mb-2 text-center">{icon}</div>
